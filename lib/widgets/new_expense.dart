3e0813d27bac979cd712e65f87f01d9490f0c700
ryansyrl <m.ridwan.riansyah@gmail.com>
2023-12-20 11:43:29 +0700

    feat: add dropdown to pick category

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -14,6 +14,7 @@ class _NewExpanseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
   DateTime? _selectedDate;
+  Category _selectedCategory = Category.leisure;
 
   void _presetDatePicker() async {
     final now = DateTime.now();
@@ -79,8 +80,29 @@ class _NewExpanseState extends State<NewExpense> {
               )
             ],
           ),
+          const SizedBox(height: 16),
           Row(
             children: [
+              DropdownButton(
+                value: _selectedCategory,
+                items: Category.values
+                    .map(
+                      (category) => DropdownMenuItem(
+                        value: category,
+                        child: Text(
+                          category.name.toUpperCase(),
+                        ),
+                      ),
+                    )
+                    .toList(),
+                onChanged: (value) {
+                  if (value == null) {
+                    return;
+                  }
+                  setState(() => _selectedCategory = value);
+                },
+              ),
+              const Spacer(),
               TextButton(
                 onPressed: () => Navigator.pop(context),
                 child: const Text('Cancel'),
