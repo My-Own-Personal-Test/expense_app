31adc06897deebbd96ad14b7cf66f66dff64f893
ryansyrl <m.ridwan.riansyah@gmail.com>
2023-12-20 19:31:09 +0700

    feat: user can add new expenses

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -31,8 +31,12 @@ class _ExpensesState extends State<Expenses> {
   void _openOverlay() {
     showModalBottomSheet(
       context: context,
-      builder: (ctx) => const NewExpense(),
+      builder: (ctx) => NewExpense(onAddExpense: _addExpense),
     );
+  }
+
+  void _addExpense(Expense expense) {
+    setState(() => _registeredExpenses.add(expense));
   }
 
   @override
